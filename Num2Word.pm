2eb222c8d6e486f0e22571e56425a17324be4f82
Roman Vašíček <rvasicek@cpan.org>
2004-06-09 12:09:13 +0000

    Import of RVASICEK/Lingua-DE-Num2Word-0.02 from CPAN.
    gitpan-cpan-distribution: Lingua-DE-Num2Word
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         RVASICEK/Lingua-DE-Num2Word-0.02.tar.gz
    gitpan-cpan-author:       RVASICEK
    gitpan-cpan-maturity:     released

--- Num2Word.pm
+++ Num2Word.pm
@@ -1,69 +1,79 @@
-# For Emacs: -*- mode:cperl; mode:folding coding:iso-8859-1 -*-
+# For Emacs: -*- mode:cperl; mode:folding; coding:iso-8859-1 -*-
 #
-# Started by rv@petamem.com at 2002-07-11
+# (c) 2002-2004 PetaMem, s.r.o.
 #
-# $Id: Num2Word.pm,v 1.4 2002/07/12 14:03:38 rv Exp $
-#
-# PPCG: 0.5
+# PPCG: 0.7
 
 package Lingua::DE::Num2Word;
 
 use strict;
 
+# {{{ BEGIN
+#
 BEGIN {
   use Exporter ();
-  use vars qw($VERSION @ISA @EXPORT_OK);
-  $VERSION = '0.01';
-  @ISA     = qw(Exporter);
-  @EXPORT_OK = qw(&num2de_cardinal);
+  use vars qw($VERSION $REVISION @ISA @EXPORT_OK);
+  $VERSION    = '0.02';
+  ($REVISION) = '$Revision: 1.8 $' =~ /([\d.]+)/;
+  @ISA        = qw(Exporter);
+  @EXPORT_OK  = qw(&num2de_cardinal);
 }
+# }}}
 
 # {{{ num2de_cardinal                 convert number to text
-
+#
 sub num2de_cardinal {
   my $positive = shift;
   my $out;
 
   my @tokens1 = qw(null ein zwei drei vier fünf sechs sieben acht neun zehn elf zwölf);
   my @tokens2 = qw(zwanzig dreissig vierzig fünfzig sechzig siebzig achtzig neunzig hundert);
-#    my @tokens3 = qw(million milliarde billion billiarde); # Not needed now
 
-  if($positive >= 0 && $positive <= 12) {
-    $out = $tokens1[$positive];
-  } elsif($positive >= 13 && $positive <= 19) {
-    $out = $tokens1[$positive-10].'zehn';
-  } elsif($positive >= 20 && $positive <= 100) {
-    my $one_idx = int $positive/10-2;
-    my $ten_idx = $positive-($one_idx+2)*10;
+  return $tokens1[$positive]           if($positive >= 0 && $positive < 13); # 0 .. 12
+  return 'sechzehn'                    if($positive == 16);                  # 16 exception
+  return 'siebzehn'                    if($positive == 17);                  # 17 exception
+  return $tokens1[$positive-10].'zehn' if($positive > 12 && $positive < 20); # 13 .. 19
 
-    $out = $tokens1[$ten_idx].'und' if $ten_idx;
-    $out .= $tokens2[$one_idx];
-  } elsif($positive >= 101 && $positive <= 999) {
-    my $one_idx = int $positive/100;
+  my $out;          # string for return value construction
+  my $one_idx;      # index for tokens1 array
+  my $remain;       # remainder
 
-    $out = $tokens1[$one_idx].'hundert'.&num2de_cardinal($positive-$one_idx*100);
-  } elsif($positive >= 1000 && $positive <= 999999) {
-    my $one_idx = int $positive/1000;
-    my $tausend = $positive-$one_idx*1000;
-    my $nonull = $tausend ? &num2de_cardinal($tausend) : '';
+  if($positive > 19 && $positive < 101) {              # 20 .. 100
+    $one_idx = int ($positive / 10);
+    $remain = $positive % 10;
 
-    $out = &num2de_cardinal($one_idx).'tausend'.$nonull;
-  } elsif($positive >= 1000000 && $positive <= 999999999) {
-    my $one_idx = int $positive/1000000;
-    my $mio     = $positive-$one_idx*1000000;
-    my $nonull  = $mio ? ' '.&num2de_cardinal($mio) : '';
-    my $one     = $one_idx == 1 ? 'e' : '';
+    $out = "$tokens1[$remain]und" if $remain;
+    $out .= $tokens2[$one_idx-2];
 
-    $out = &num2de_cardinal($one_idx).$one.' million';
+  } elsif($positive > 100 && $positive < 1000) {       # 101 .. 999
+    $one_idx = int ($positive / 100);
+    $remain  = $positive % 100;
+
+    $out  = "$tokens1[$one_idx]hundert";
+    $out .= $remain ? &num2de_cardinal($remain) : '';
+
+  } elsif($positive > 999 && $positive < 1_000_000) {  # 1000 .. 999_999
+    $one_idx = int ($positive / 1000);
+    $remain  = $positive % 1000;
+
+    $out  = &num2de_cardinal($one_idx).'tausend';
+    $out .= $remain ? &num2de_cardinal($remain) : '';
+
+  } elsif($positive > 999_999 &&
+	  $positive < 1_000_000_000) {                 # 1_000_000 .. 999_999_999
+    $one_idx = int ($positive / 1000000);
+    $remain  = $positive % 1000000;
+    my $one  = $one_idx == 1 ? 'e' : '';
+
+    $out = &num2de_cardinal($one_idx)."$one million";
     $out .= 'en' if $one_idx > 1;
-    $out .= $nonull;
+    $out .= $remain ? ' '.&num2de_cardinal($remain) : '';
   }
 
   return $out;
 }
 
 # }}}
-
 
 1;
 __END__
@@ -113,7 +123,7 @@ Roman Vasicek E<lt>rv@petamem.comE<gt>
 
 =head1 COPYRIGHT
 
-Copyright (c) 2002 PetaMem s.r.o.
+Copyright (c) 2002-2004 PetaMem s.r.o.
 
 This package is free software. Tou can redistribute and/or modify it under
 the same terms as Perl itself.
@@ -121,5 +131,6 @@ the same terms as Perl itself.
 =cut
 
 # }}}
+
 
 
